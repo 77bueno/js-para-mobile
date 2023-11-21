a3f7eee2c9400269453d4a2adfa1186db1a1c0a0
victor <124385408+77bueno@users.noreply.github.com>
2023-11-21 09:47:38 -0300

    Função anônima e Função nomeada

---
+++ 02-funcoes.js
@@ -0,0 +1,19 @@
+/* Formas tradicionais */
+
+/* Sintaxe anônima associada à variável/constante
+Obs.: cuidado com a ordem, já que nesta sintaxe a função PRIMEIRO
+precisa ser declarada para somente DEPOIS ser chamada/executada */
+const exemplo1 = function(){
+    console.log("Função anônima!");
+};
+
+exemplo1();
+
+/* Sintaxe declarada/nomeada 
+Obs.: nesta sintaxe, você pode chamar a função
+antes ou depois de declará-la. Portanto, a ordem não importa */
+function exemplo2(){
+    console.log("Função nomeada!");
+};
+
+exemplo2();
