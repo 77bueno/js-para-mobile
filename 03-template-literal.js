1beb56f743cd4c1a5ea6af4cf3eb8259ef872b4f
victor <124385408+77bueno@users.noreply.github.com>
2023-11-22 08:54:43 -0300

    Concatenação - exemplo usando \  - "escape"

---
+++ 03-template-literal.js
@@ -0,0 +1,12 @@
+const nome = "Chapolin";
+const sobrenome = "Colorado";
+let idade = "25";
+let cidade = "Acapulco";
+let estado = "Guerrero";
+
+/* Concatenação tradicional usando operador + 
+Meu nome é Chapolin Colorado, tenho 25 anos e moro atualmente 
+na cidade de São Paulo - SP. */
+let mensagem1 = "Meu nome é <b class=\"destaque\">"+nome+"</b> "+sobrenome+", tenho "+idade+" anos e moro atualmente na cidade de "+cidade+" - "+estado+".";
+
+console.log(mensagem1);
