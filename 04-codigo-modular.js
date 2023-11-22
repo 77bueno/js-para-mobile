3fec90da0ba35fc31af022c4e90240fce4dfe1ec
victor <124385408+77bueno@users.noreply.github.com>
2023-11-22 09:28:29 -0300

    Modulos: exportando e importando, e criando o package.json

---
+++ 04-codigo-modular.js
@@ -0,0 +1,10 @@
+import pessoa from "./modulos/dados.js";
+
+console.log(pessoa);
+console.log(pessoa.nome);
+
+console.log("-------------------------------------------");
+
+for (const prop in pessoa) {
+    console.log(pessoa[prop]);
+}
---
+++ modulos/dados.js
@@ -0,0 +1,7 @@
+const pessoa = {
+    nome: "Jon Oliva",
+    idade: 66,
+    cidade: "São Paulo"
+};
+
+export default pessoa;
