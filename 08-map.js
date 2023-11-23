308aa857d1122c7de97647c3a9550b5992c30fac
victor <124385408+77bueno@users.noreply.github.com>
2023-11-23 10:56:52 -0300

    Exemplo 2: gerar um novo array, usando callback's

--- 08-map.js
+++ 08-map.js
@@ -1,6 +1,8 @@
+import cursos from "./modulos/cursos.js"
+
 /* Map (mapear, mapeia - transformar em outra coisa)
 Passa por elementos de um array e realiza operações 
-em cada um deles através de uma função, gerando um novo array. */
+em cada um deles através de uma função (callback), gerando um novo array. */
 
 // Exemplo 1
 const numeros = [10, 20, 5, 12, 65, 50];
@@ -9,3 +11,16 @@ const numerosDobrados = numeros.map(
 );
 console.log(numeros);
 console.log(numerosDobrados);
+
+console.log("------------------------------------------------");
+
+
+console.log(cursos);
+
+// Exemplo 2: gerar um novo array apenas com os nomes dos cursos
+
+const titulos = cursos.map( (curso) => {
+    return curso.titulo
+} );
+
+console.log(titulos);
