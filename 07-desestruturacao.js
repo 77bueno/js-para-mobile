ab2c9203168e5bc4675b43b8a655b2360e005e2f
victor <124385408+77bueno@users.noreply.github.com>
2023-11-23 09:51:27 -0300

    Desestruturação com array, acessando atraves da constante. Desestruturação em objetos, e colocando apelido para as propriedades

--- 07-desestruturacao.js
+++ 07-desestruturacao.js
@@ -26,3 +26,54 @@ const [tesla, einstein, newton] = ["Tesla", "Einstein", "Newton"];
 console.log(tesla);
 console.log(einstein);
 console.log(newton);
+
+console.log("--------------------------------------------------------");
+
+const recursos = [
+    ["Notebook", "TV Led", "Computador Desktop"],
+    texto => texto.toUpperCase()
+];
+ 
+console.log(recursos); // Array completo
+
+// Destructuring
+const [produtos, converter] = recursos;
+
+// Acessando através da constante que foi gerada pelo destructuring
+console.log(produtos[1]); // TV Led
+
+console.log("-----------------");
+
+for (const produto of produtos) {
+    console.log("Produto: "+produto);
+}
+
+console.log( converter("Victor") );
+console.log( converter("JhOnAs") );
+console.log( converter(produtos[2]) );
+
+
+console.log("--------------------------------------------------------");
+
+
+// Destructuring em Objetos
+const pessoa = {
+    nome: "André",
+    idade: 19,
+    bairro: "Penha",
+    situacao: "crítica"
+};
+
+const { nome, idade, bairro, situacao } = pessoa;
+
+console.log(`O aluno ${nome} está em situação ${situacao} no curso.`);
+console.log(`Mora na ${bairro} e mesmo assim sempre atrasa... ;(`);
+
+// Criando um apelido para a propriedade codigo
+const { codigo: pedido, cursos, preco } = {
+    codigo: "123abc", cursos: ["Figma", "Node.js"], preco: 1000 
+};
+
+console.log(pedido); // Pedido é um apelido
+console.log(cursos);
+console.log(preco);
