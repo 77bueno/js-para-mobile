6186dc271c8995b0cbc510ec786b86c448465d42
victor <124385408+77bueno@users.noreply.github.com>
2023-11-24 09:05:04 -0300

    Exemplo 2: filter por letras de nomes

--- 09-filter.js
+++ 09-filter.js
@@ -1,3 +1,4 @@
+import cursos from "./modulos/cursos.js";
 /* Filter (filtrar) 
 Passa por elementos de um array (usando uma função callback),
 e retornar valores de acordo com uma ou mais condições gerando um novo array.*/
@@ -13,3 +14,25 @@ const vendasAcimadaMeta = vendas.filter( (venda) => {
 
 console.log(vendas);
 console.log(vendasAcimadaMeta);
+
+// Exemplo 2
+const alunos = [
+    "Nicolas", "Eliel", "André", "Kaue", 
+    "Aline", "Melissa", "Marina", "Victor", 
+    "Mônica", "Marcelo", "Jhonas"
+];
+
+// Filtrando alunos que começam com "M" e terminam com "O"
+// Obs.: faz diferença usar maiúsculas ou minúsculas.
+const resultados = alunos.filter( aluno => aluno.startsWith("M") && aluno.endsWith("o") );
+
+console.log(resultados);
+
+console.log("-----------------------------------------");
+
+/* Filtrando cursos */
+
+// Filtrando por cursos da categoria Design
+const cursosDesign = cursos.filter( curso => curso.categoria == "Design" );
+
+console.log(cursosDesign);
